0e528fee9f35e0ad69745a48f79ff464903cb0fc
Jacob Stolberg <jacobstolberg@Jacobs-MacBook-Air.local>
2019-05-30 01:21:36 -0500

    Booking controller and view made routes done

--- controllers/BookingController.rb
+++ controllers/BookingController.rb
@@ -1,3 +1,29 @@
 class BookingController < ApplicationController
+	get '/new'
+		band = Band.find params[:id]
+		show = Show.find params [:id]
+		if band && !show
+			shows = band.shows
+			erb :booking_new
+		end
+		erb :booking_new
+	end
 
+	post '/bookings' do 
+		#build it
+		booking = Booking.new
+		#set values
+		booking.venue = params[venue]
+		booking.band_id = session[:id] #add line in login that adds the band id to the session obj
+		#booking.show_id = params
+#we need to get the show_id based on the show they click in the dropdown
+
+
+
+		booking.save
+		session[:message]={
+			success: true,
+			message: "You have been booked! Break a leg!"
+		}
+	end
 end
